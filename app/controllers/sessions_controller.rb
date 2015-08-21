ebd71740fea759ddf2efd37a1b925a53f0047de3
Oscar+Arthur <oscar+arthur@aa.io>
2015-08-21 11:18:14 -0400

    Complete Auth functionalities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_credentials(params[:username], params[:password])
+    # fail
     if user
       login!(user)
       redirect_to user_url
     else
+      flash[:notice] = "Invalid username or password."
       redirect_to new_session_url
     end
   end
 
   def destroy
+    logout!
+    redirect_to new_session_url
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,28 @@
 class UsersController < ApplicationController
-  def create
-    # insert an user entry with a token
-    # login
+
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      redirect_to user_url
+    else
+      # flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
